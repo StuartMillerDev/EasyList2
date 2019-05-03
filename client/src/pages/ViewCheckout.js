ed0e2c3ff6a28c94889adbd5d66c016a9ece6920
johndoeadams <johndoeadams@gmail.com>
2019-05-02 21:08:07 -0400

    more checkout updates

--- client/src/pages/ViewCheckout.js
+++ client/src/pages/ViewCheckout.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import List from "../components/List";
+import { Link } from 'react-router-dom'
 import Banner from "../components/Banner";
 
 class ViewCheckout extends Component{
@@ -16,9 +17,9 @@ class ViewCheckout extends Component{
              <div className="text-center">
                  <Banner listName={"Checkout"}  text={"CHECKOUT"}/>
 
-                 YOU HAVE CHECKED OUT
+                 <h3>YOU HAVE CHECKED OUT</h3>
 
-                 <div className="">RETURN TO CONTROL</div>
+                 <div className="" id="returnToControl"><Link  sytle={{background:"#F26419",color: '#FFF'}} to="/control">RETURN TO CONTROL</Link></div>
                  
               </div>
           );
